da64eb27ac1c494619725f717a76308f2d43c66d
sansegundo <ben@sansegundo.de>
2026-01-27 22:07:57 +0100

    upgrade to fxmanifest

--- lightbars/fxmanifest.lua
+++ lightbars/fxmanifest.lua
@@ -1,6 +1,5 @@
--- Version 0.1
--- Devloped by Everett aka Mr. Yellow aka Munky aka De_verett
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+fx_version 'cerulean'
+game 'gta5'
 
 files {
 	'data/vehicles.meta',
