fd18d5ee8bac585475a222249dbf6a2a944338c2
sansegundo <ben@sansegundo.de>
2026-01-27 22:20:13 +0100

    fix nui callbacks

--- kn_lightbar/html/script.js
+++ kn_lightbar/html/script.js
@@ -1,32 +1,44 @@
+const resourceName = (typeof GetParentResourceName === "function" && GetParentResourceName()) || "kn_lightbar";
+
+function postToResource(endpoint, payload)
+{
+  $.post(`https://${resourceName}/${endpoint}`, JSON.stringify(payload || {}));
+}
+
 $(function()
 {
-    $(document).keyup(function (e) {
-        if (e.keyCode == 27) {
-            $('#container').fadeOut();
-            $('#admin').fadeOut();
-            $.post('https://kn_lightbar/close', JSON.stringify({}));
-        }
-    });
-    
-    //get info from client side
-    window.addEventListener('message', function(event)
+  $(document).keyup(function(e)
+  {
+    if (e.keyCode == 27)
     {
-        var kn = event.data;
-        if (kn.action == 'open') {
-            $('#container').fadeIn();
-            $('#players').html(kn.players);
-        }
+      $('#container').fadeOut();
+      $('#admin').fadeOut();
+      postToResource('close');
+    }
+  });
 
-    }, false);
+  //get info from client side
+  window.addEventListener('message', function(event)
+  {
+    var kn = event.data;
+    if (kn.action == 'open')
+    {
+      $('#container').fadeIn();
+      $('#players').html(kn.players);
+    }
 
-    $("#back").click(() => {
-        $('#admin').css('display', 'none');
-        $('#container').css('display', 'block');
-    });
+  }, false);
+
+  $("#back").click(() =>
+  {
+    $('#admin').css('display', 'none');
+    $('#container').css('display', 'block');
+  });
 
 });
 
-function OpenMenu(type) {
-    $('#container').fadeOut();
-    $.post('https://kn_lightbar/use', JSON.stringify({ type: type}));
+function OpenMenu(type)
+{
+  $('#container').fadeOut();
+  postToResource('use', { type: type });
 }
